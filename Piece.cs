838bb16ddb822170fb2959bec0c9f5cf8621369d
Itamar Pele <itamarpele@gmail.com>
2023-10-06 13:04:15 +0300

    testing

--- Piece.cs
+++ Piece.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,6 @@ namespace MyChess
 {
     internal class Piece
     {
-
+        abstract async CallConvFastcall;
     }
 }
